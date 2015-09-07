7017c3bf05bc0b90b40913fa78257dd0a4395dd3
pewho <pewhoo@gmail.com>
2015-09-07 16:02:58 +0200

    Modify appComponent to show errors

--- src/components/AppComponent.js
+++ src/components/AppComponent.js
@@ -6,29 +6,59 @@ var FileStore = require('../stores/FileStore');
 var vfConf = require('../api/api');
 
 var AppComponent = React.createClass({
+	getInitialState: function() {
+		return {
+			errorMsg: null
+		};
+	},
 	componentWillMount: function(){
-		FileStore.initStore();
 		FileStore.on('ERROR', this.logError);
+		FileStore.initStore();
 	},
 	logError: function(err){
 		console.log(err);
+		this.setState({
+			errorMsg: err
+		});
 	},
 	render: function() {
 		var uploadElement = <span></span>;
+		var mainElement = <span></span>;
 
 		if (vfConf !== undefined && vfConf.appName !== 'NEURONAUTE'){
 			uploadElement = <UploadComponent />;
 		}
-		
-		return (
-			<div className="app-component">
-				<div className="pure-g">
-					<div className="pure-u-1 button-container">
-						<button className="pure-button" id="return-to-record" onClick = {this.returnRecord}>&lt; Return to Record</button>
+
+		if (this.state.errorMsg){
+			mainElement = (
+				<div className="app-component">
+					<div className="pure-g l-box-error">
+						<div className="pure-u-1">
+							 { this.state.errorMsg.message }
+						</div>
 					</div>
 				</div>
-				<LinksComponent />
-				{ uploadElement }
+			);
+			console.log(mainElement);
+		} else {
+			mainElement = (
+				<div className="app-component">
+					<div className="pure-g">
+						<div className="pure-u-1 button-container">
+							<button className="pure-button" id="return-to-record" onClick = {this.returnRecord}>&lt; Return to Record</button>
+						</div>
+					</div>
+
+					<LinksComponent />
+
+					{ uploadElement }
+				</div>
+			);
+		}
+
+		return (
+			<div>
+			{ mainElement }
 			</div>
 		);
 	},
